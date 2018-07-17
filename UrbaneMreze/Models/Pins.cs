9461691760c59a1e1e674a94fb9d378aef700a88
Gašper Sinur <gasper.sinur@gmail.com>
2018-07-17 13:20:28 +0200

    A

---
+++ UrbaneMreze/Models/Pins.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UrbaneMreze.Models
+{
+    public class Pins
+    {
+        [Key]
+        public Guid PinGuid { get; set; }
+
+        public string Name { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Color { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
