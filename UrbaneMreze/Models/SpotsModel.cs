7ee3c9c884ea4d562daea3e5eac796f51ea03ff5
Gašper Sinur <gasper.sinur@gmail.com>
2018-09-12 09:28:11 +0200

    Connection string / footer

--- UrbaneMreze/Models/SpotsModel.cs
+++ UrbaneMreze/Models/SpotsModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -43,6 +44,9 @@ namespace UrbaneMreze.Models
 
         [Display(Name = "Avtor spremembe")]
         public Guid UserModifiedID { get; set; }
+
+        [NotMapped]
+        public String SpotAuthorUsername { get; set; }
     }
 
     public class SpotEditViewModel
